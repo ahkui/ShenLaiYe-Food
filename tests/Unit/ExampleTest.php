cccd6c525a9ae664a6ce293784c4ca49a9dd26dc
KUI\ahkui <ahkui@outlook.com>
2018-01-14 19:01:50 +0800

    update

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -3,6 +3,8 @@
 namespace Tests\Unit;
 
 use Tests\TestCase;
+use App\User;
+use App\SearchResult;
 
 class ExampleTest extends TestCase
 {
@@ -14,17 +16,16 @@ class ExampleTest extends TestCase
     public function testBasicTest()
     {
         $password5 = str_random(5);
-        $response = $this->json('POST', 'register', [
+        $this->json('POST', 'register', [
             "name" => "$password5",
             "email" => "$password5@$password5.com",
             "password" => "$password5",
             "password_confirmation" => "$password5",
             "_token" => csrf_token(),
-        ]);
-        $response->assertStatus(422);
+        ])->assertStatus(422);
         $password = str_random(6);
         
-        $response = $this->json('POST', 'register', [
+        $this->json('POST', 'register', [
             "name" => "$password",
             "email" => "$password@Sally.com",
             "password" => "$password",
@@ -33,19 +34,62 @@ class ExampleTest extends TestCase
         ])->assertStatus(302);
 
         
-        $response = $this->json('POST', 'login', [
+        $this->json('POST', 'login', [
             "email" => "$password@Sally.com",
-            "password" => "$password",
+            "password" => "$password5",
             "_token" => csrf_token(),
         ])->assertStatus(302);
 
-        $response = $this->json('POST', 'login', [
-            "email" => "$password@Sally.com",
-            "password" => "asd",
-            "_token" => csrf_token(),
-        ])->assertStatus(302);
+        $this
+            ->actingAs(User::first())
+            ->get('/')
+            ->assertStatus(200);
+        $this
+            ->actingAs(User::first())
+            ->json('POST','search',['name'=>"小阿姨"])
+            ->assertJson(SearchResult::where('keyword', 'like', "小阿姨")->get()->toArray());
+        $this
+            ->actingAs(User::first())
+            ->json('POST','search',['name'=>"小阿姨"])
+            ->assertJson(SearchResult::where('keyword', 'like', "小阿姨")->get()->toArray());
+            // ->assertStatus(200);
+        foreach(SearchResult::get() as $value){
+            $value->delete();
+        }
+        $this
+            ->actingAs(User::first())
+            ->json('POST','search/gps',[
+                    'longitude'=>120.646705,
+                    'latitude'=>24.178820,
+                ])
+            ->assertJson(SearchResult::get()->toArray());
+        $this
+            ->actingAs(User::first())
+            ->json('POST','review',[
+                    'longitude'=>120.646705,
+                    'latitude'=>24.178820,
+                ])
+            ->assertJson([[
+                    'longitude'=>120.646705,
+                    'latitude'=>24.178820,
+                ]]);
 
-        $response = $this->json('POST', 'logout', [])->assertStatus(302);
+        // $this
+        //     ->actingAs(User::first())
+        //     ->json('POST','search',['name'=>"小阿姨"])
+        // $response = $this->json('POST', 'login', [
+        //     "email" => "$password@Sally.com",
+        //     "password" => "$password5"."wsqer",
+        //     "_token" => csrf_token(),
+        // ])->assertStatus(302);
+
+        // $response = $this->json('POST', 'login', [
+        //     "email" => "$password@Sally.com",
+        //     "password" => "asd",
+        //     "_token" => csrf_token(),
+        // ])->assertStatus(302);
+
+        // $response = $this->json('POST', 'logout', [])->assertStatus(302);
 
     }
 }
