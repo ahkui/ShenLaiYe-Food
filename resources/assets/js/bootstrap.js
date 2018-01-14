0316ea7b488f7da06398377802763de47066cdc1
KUI\ahkui <ahkui@outlook.com>
2018-01-14 19:12:54 +0800

    update

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -8,8 +8,6 @@ window._ = require('lodash');
 
 try {
     window.$ = window.jQuery = require('jquery');
-    require("jquery-mousewheel")($);
-    require('malihu-custom-scrollbar-plugin')($);
     window.Popper = require('popper.js').default;
     require('bootstrap');
 } catch (e) {}
