da56bd895e760b9186b91bb4ef8eba61168e4b50
joonkui95 <ahkui@outlook.com>
2018-01-14 11:04:06 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -2,9 +2,9 @@
 
 namespace Tests\Unit;
 
-use Tests\TestCase;
-use App\User;
 use App\SearchResult;
+use App\User;
+use Tests\TestCase;
 
 class ExampleTest extends TestCase
 {
@@ -17,27 +17,26 @@ class ExampleTest extends TestCase
     {
         $password5 = str_random(5);
         $this->json('POST', 'register', [
-            "name" => "$password5",
-            "email" => "$password5@$password5.com",
-            "password" => "$password5",
-            "password_confirmation" => "$password5",
-            "_token" => csrf_token(),
+            'name'                  => "$password5",
+            'email'                 => "$password5@$password5.com",
+            'password'              => "$password5",
+            'password_confirmation' => "$password5",
+            '_token'                => csrf_token(),
         ])->assertStatus(422);
         $password = str_random(6);
-        
+
         $this->json('POST', 'register', [
-            "name" => "$password",
-            "email" => "$password@Sally.com",
-            "password" => "$password",
-            "password_confirmation" => "$password",
-            "_token" => csrf_token(),
+            'name'                  => "$password",
+            'email'                 => "$password@Sally.com",
+            'password'              => "$password",
+            'password_confirmation' => "$password",
+            '_token'                => csrf_token(),
         ])->assertStatus(302);
 
-        
         $this->json('POST', 'login', [
-            "email" => "$password@Sally.com",
-            "password" => "$password5",
-            "_token" => csrf_token(),
+            'email'    => "$password@Sally.com",
+            'password' => "$password5",
+            '_token'   => csrf_token(),
         ])->assertStatus(302);
 
         $this
@@ -46,32 +45,32 @@ class ExampleTest extends TestCase
             ->assertStatus(200);
         $this
             ->actingAs(User::first())
-            ->json('POST','search',['name'=>"小阿姨"])
-            ->assertJson(SearchResult::where('keyword', 'like', "小阿姨")->get()->toArray());
+            ->json('POST', 'search', ['name'=>'小阿姨'])
+            ->assertJson(SearchResult::where('keyword', 'like', '小阿姨')->get()->toArray());
         $this
             ->actingAs(User::first())
-            ->json('POST','search',['name'=>"小阿姨"])
-            ->assertJson(SearchResult::where('keyword', 'like', "小阿姨")->get()->toArray());
-            // ->assertStatus(200);
-        foreach(SearchResult::get() as $value){
+            ->json('POST', 'search', ['name'=>'小阿姨'])
+            ->assertJson(SearchResult::where('keyword', 'like', '小阿姨')->get()->toArray());
+        // ->assertStatus(200);
+        foreach (SearchResult::get() as $value) {
             $value->delete();
         }
         $this
             ->actingAs(User::first())
-            ->json('POST','search/gps',[
-                    'longitude'=>120.646705,
-                    'latitude'=>24.178820,
+            ->json('POST', 'search/gps', [
+                    'longitude'=> 120.646705,
+                    'latitude' => 24.178820,
                 ])
             ->assertJson(SearchResult::get()->toArray());
         $this
             ->actingAs(User::first())
-            ->json('POST','review',[
-                    'longitude'=>120.646705,
-                    'latitude'=>24.178820,
+            ->json('POST', 'review', [
+                    'longitude'=> 120.646705,
+                    'latitude' => 24.178820,
                 ])
             ->assertJson([[
-                    'longitude'=>120.646705,
-                    'latitude'=>24.178820,
+                    'longitude'=> 120.646705,
+                    'latitude' => 24.178820,
                 ]]);
 
         // $this
@@ -90,6 +89,5 @@ class ExampleTest extends TestCase
         // ])->assertStatus(302);
 
         // $response = $this->json('POST', 'logout', [])->assertStatus(302);
-
     }
 }
